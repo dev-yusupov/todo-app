ac40d260fa57b3a9fc840cff18a5de0ee187b86d
Bobur Yusupov <boburinfo2006@gmail.com>
2023-07-25 11:33:35 +0500

    Completed 1st version

--- backend/todo/views/task_views.py
+++ backend/todo/views/task_views.py
@@ -9,6 +9,7 @@ from rest_framework.mixins import (
     DestroyModelMixin,
 )
 from rest_framework.authentication import TokenAuthentication
+from dj_rest_auth.jwt_auth import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from todo.models.task.model import Task
@@ -17,5 +18,30 @@ from todo.serializers.task.serializer import TaskSerializer
 class TaskViewSet(ModelViewSet):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
-    authentication_classes = (TokenAuthentication,)
+    authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAuthenticated,)
+
+    def _params_to_ints(self, qs):
+        """Convert a list of strings into integers."""
+        return [int(str_id) for str_id in qs.split(',')]
+
+    def get_queryset(self):
+        """Retrieve recipes for authenticated user."""
+        # return self.queryset.filter(user=self.request.user).order_by("-id")
+        queryset = self.queryset
+        
+        return queryset.filter(
+            user=self.request.user
+        ).order_by("-id").distinct()
+    
+    def get_serializer_class(self):
+        """Return the serializer class for request."""
+        if self.action == "list":
+            return TaskSerializer
+        
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)
+    
