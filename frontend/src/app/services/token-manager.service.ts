7b667f7e97f07348c825af5cc41d8471ea40de38
Bobur Yusupov <boburinfo2006@gmail.com>
2023-07-26 17:29:57 +0500

    Added some changes

--- frontend/src/app/services/token-manager.service.ts
+++ frontend/src/app/services/token-manager.service.ts
@@ -1,9 +1,32 @@
+import { HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { JwtHelperService, } from '@auth0/angular-jwt'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenManagerService {
 
-  constructor() { }
+  private _jwtHelper = JwtHelperService;
+
+  constructor( private jwtHelper: JwtHelperService ) { }
+
+  isExpired(accessToken: string): boolean {
+    return this.jwtHelper.isTokenExpired(accessToken);
+  }
+
+  getToken() {
+    const token = localStorage.getItem('token');
+
+    if(token) {
+      return JSON.parse(token);
+    }
+    return null;
+  }
+
+  // addTokenHeader(request: HttpRequest<any>, token: string) {
+  //   return request.clone({
+  //     headers: request.headers.set(TOKEN)
+  //   })
+  // }
 }
